98b2d8b83e0c10005c559dabaa540d7a8bec60bf
Jay German <jsgerman@me.com>
2014-12-08 15:46:44 -0800

    Updating combobox.spec

--- test/directives/bossy.combobox.spec.js
+++ test/directives/bossy.combobox.spec.js
@@ -1,34 +1,33 @@
 describe('bossyCombobox',function() {
   var myFunction = BC.myFunction;
 
-  // Need to setup beforeEach Statements
-  beforeEach(function(){
-  
-  });
+  // If we need to setup beforeEach Statements
+  //beforeEach(function(){ });
 
   // Do we need anything after each test?
- // afterEach(function(){ });
+  // afterEach(function(){ });
  
 
   // MultiSelect
   // Select Multiple objects in list
     it("Should be able to click multiple objects in list", function() {
       expect(mSelect()).tobetrue();
-
-
-  });
+    });
 
   // None Selected
     it("Should have all elements unchecked in list", function() {
       expect(mNoSelected()).tobetrue();
-
     });
 
   
   // All Selected
     it("Should have all elements checked in list", function() {
       expect(mAllSelected()).tobetrue();
+    });
 
+  // Check Strings
+    it("", function() {
+      expect(mCheckString()).tobetrue();
     });
 
 
