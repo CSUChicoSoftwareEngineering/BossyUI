7387d19e57d193c41fb053eef14e30b4ce0cf3c4
Sean German <jsgerman@me.com>
2014-12-08 14:11:31 -0800

    Added what I am working on for testing. Definitely Not complete but might help.

---
+++ test/directives/bossy.combobox.spec.js
@@ -0,0 +1,30 @@
+describe('bossyCombobox',function() {
+  var myFunction = BC.myFunction;
+
+  // Need to setup beforeEach Statements
+  beforeEach(function(){
+  
+  });
+
+  // Do we need anything after each test?
+ // afterEach(function(){ });
+ 
+
+  // MultiSelect
+  // Select Multiple objects in list
+    it("Should be able to click multiple objects in list", function() {
+      expect
+
+
+  });
+
+  // None Selected
+    it("Should
+
+  
+  
+  // All Selected
+  
+
+
+}
